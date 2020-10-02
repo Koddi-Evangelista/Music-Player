7d7cd0897c8895f9e9a1f9926732bedebf4bcae2
Angelo Koddi <angelo.koddi@gmail.com>
2020-10-02 12:42:34 +0800

    adding more image for songs and functionality of music app

--- src/components/MusicNavigation.vue
+++ src/components/MusicNavigation.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="musicNav">
     <div class="img">
-      <img src="@/assets/bg.jpg" alt="" />
+      <img :src="songPlaying.cover" alt="" />
     </div>
-    <audio controls id="aud">
-      <source :src="require(`@/assets/media/${audio}`)" type="audio/mp3" />
-    </audio>
     <div class="buttons">
       <div class="button prev" tabindex="0" @click="prev">
         <img src="@/assets/prev.png" alt="" />
@@ -29,28 +26,54 @@
       <div class="button next" tabindex="0" @click="next">
         <img src="@/assets/next.png" alt="" />
       </div>
+      <div class="button next" tabindex="0" @click="volume">
+        <img :src="volumeSrc" alt="" />
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { mapGetters, mapMutations } from "vuex";
-const SLUG = document.getElementById("aud");
+import { mapGetters } from "vuex";
 export default {
   data() {
     return {
-      playButtonClicked: false
+      playButtonClicked: false,
+      player: new Audio(),
+      volumeVal: 0.9999999999990001,
+      volumeButtonClicked: 3,
     };
   },
   methods: {
-    ...mapMutations(["prev", "next"]),
+    prev() {
+      this.$store.commit("prev");
+      this.player.src = this.audio;
+      this.play();
+    },
+    next() {
+      this.$store.commit("next");
+      this.player.src = this.audio;
+      this.play();
+    },
     play() {
-      SLUG.play();
+      this.player.play();
       this.playButtonClicked = true;
     },
     pause() {
-      SLUG.pause();
+      this.player.pause();
       this.playButtonClicked = false;
-    }
+    },
+    volume() {
+      if (this.volumeVal === 0.9999999999990001) {
+        this.volumeVal = 0;
+        this.player.volume = this.volumeVal;
+        this.volumeButtonClicked = 0;
+      } else {
+        this.volumeVal += 0.333333333333;
+        this.player.volume = this.volumeVal;
+        this.volumeButtonClicked++;
+      }
+      console.log(this.volumeButtonClicked);
+    },
   },
   computed: {
     ...mapGetters(["songPlaying"]),
@@ -59,8 +82,23 @@ export default {
     },
     index() {
       return this.$store.state.index;
-    }
-  }
+    },
+    duration() {
+      return this.player.currentTime;
+    },
+    volumeSrc() {
+      if (this.volumeButtonClicked > 0) {
+        return require(`@/assets/volume${this.volumeButtonClicked}.png`);
+      }
+      return require("@/assets/mute.png");
+    },
+  },
+  created() {
+    this.player.src = this.audio;
+  },
+  update() {
+    this.player.ended(this.next);
+  },
 };
 </script>
 <style>
@@ -84,6 +122,7 @@ export default {
 .buttons {
   width: 80%;
   height: 100%;
+  padding: 0 1rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -91,12 +130,10 @@ export default {
 .button {
   width: 5%;
   height: 30%;
+  cursor: pointer;
 }
 .button img {
   width: 100%;
   height: 100%;
-}
-audio {
-  display: none;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -9,50 +9,55 @@ export default new Vuex.Store({
       {
         artist: "Sleepmakeswaves",
         song: "It's Dark, It's Cold, I'ts Winter",
-        slug: "itsdark.mp3"
+        slug: require("@/assets/itsdark.mp3"),
+        cover: require("@/assets/smwimg.jpg"),
       },
       {
         artist: "Death Grips",
         song: "Guillotine",
-        slug: "guillotine.mp3"
+        slug: require("@/assets/guillotine.mp3"),
+        cover: require("@/assets/dgimg.jpg"),
       },
       {
         artist: "Joy wants Eternity",
         song: "From Embrace to Embrace",
-        slug: "embrace.mp3"
+        slug: require("@/assets/embrace.mp3"),
+        cover: require("@/assets/jweimg.jpg"),
       },
       {
         artist: "Com Truise",
         song: "Cyanide Sisters",
-        slug: "cyanideSisters.mp3"
+        slug: require("@/assets/cyanideSisters.mp3"),
+        cover: require("@/assets/ctimg.jpg"),
       },
       {
         artist: "Best Coast",
         song: "The Only Place",
-        slug: "place.mp3"
-      }
+        slug: require("@/assets/place.mp3"),
+        cover: require("@/assets/bcimg.jpg"),
+      },
     ],
-    index: 0
+    index: 0,
   },
   getters: {
-    songPlaying: state => state.songs[state.index]
+    songPlaying: (state) => state.songs[state.index],
   },
   mutations: {
-    prev: state => {
+    prev: (state) => {
       if (state.index === 0) {
         return (state.index = state.songs.length - 1);
       } else {
         return (state.index = state.index - 1);
       }
     },
-    next: state => {
+    next: (state) => {
       if (state.index === state.songs.length - 1) {
         return (state.index = 0);
       } else {
         return (state.index = state.index + 1);
       }
-    }
+    },
   },
   actions: {},
-  modules: {}
+  modules: {},
 });
