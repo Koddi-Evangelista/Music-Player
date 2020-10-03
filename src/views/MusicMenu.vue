fc6adbb581fe3d5f26d6ee2d9820acf3f9ddfae6
Angelo Koddi <angelo.koddi@gmail.com>
2020-10-03 10:05:35 +0800

    modify playlist menu

--- src/views/MusicMenu.vue
+++ src/views/MusicMenu.vue
@@ -1,8 +1,60 @@
 <template>
-  <h1>music is on</h1>
+  <div class="music-cont">
+    <h1>PLAYLIST</h1>
+    <ul>
+      <li v-for="(song, index) in songs" :key="index">
+        <div class="song">
+          <p class="title">{{ song.song }}</p>
+          <p class="artist">{{ song.artist }}</p>
+        </div>
+        <div class="img">
+          <img :src="song.cover" alt="" />
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 <script>
+import { mapState } from "vuex";
 export default {
-  name: "MusicMenu"
+  name: "MusicMenu",
+  computed: {
+    ...mapState(["songs"])
+  }
 };
 </script>
+<style scoped>
+.music-cont {
+  margin-top: 1rem;
+  width: 100%;
+}
+h1 {
+  color: white;
+}
+ul {
+  list-style-type: none;
+  width: 80%;
+  height: 80vh;
+  margin: 1rem auto;
+  overflow: auto;
+}
+li {
+  width: 100%;
+  height: 150px;
+  background-color: rgba(0, 0, 0, 0.7);
+  padding: 0.5rem;
+  margin: 0.5rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.song {
+  max-width: 60%;
+  overflow: auto;
+  padding: 1rem 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+}
+</style>
